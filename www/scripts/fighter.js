8ccf1a2238c9b155e6b650a66a13c702434e209d
hichembou21 <hichem.bouaffar@gmail.com>
2018-01-11 18:42:49 +0100

    continué

--- www/scripts/fighter.js
+++ www/scripts/fighter.js
@@ -7,4 +7,23 @@ class Fighter {
         this.score = 0;
         this.life = 100;
     }
+
+    simpleAttack(adverser) {
+        this.score+=5;
+        this.power-=5;
+        adverser.power-=40;
+        adverser.life-=10;
+    }
+
+    superAttack (adverser) {
+        this.score+=30;
+        this.power-=30;
+        adverser.power-=80;
+        adverser.life-=20;
+    }
+
+    getPower() {
+        this.power+=5;
+    }
+
 }
--- www/scripts/game.js
+++ www/scripts/game.js
@@ -3,43 +3,65 @@
 let fighters = [
     ["ryu", 500], 
     ["ken", 500], 
-    ["ronda", 400]
+    ["ronda", 400],
+    ["andro", 400],
+    ["guile", 400]
 ]; 
+let adversers = [
+    ["ryu", 500], 
+    ["ken", 500], 
+    ["chanli", 400],
+    ["police", 400],
+    ["bison", 400]
+];
 
+let fighter = new Fighter();
+let adverser= new Fighter();
+
+// récupération des éléments Buttons 
 let buttonStart = document.querySelector('div button[type=submit]');
+let buttonAttackF = document.querySelector('#attack-f');
+let buttonAttackA = document.querySelector('#attack-a');
+let buttonGetPowerF = document.querySelector('#get-power-f');
+let buttonGetPowerA = document.querySelector('#get-power-a');
+let buttonSuperAttackF = document.querySelector('#super-attack-f');
+let buttonSuperAttackA = document.querySelector('#super-attack-a');
+let buttonJumpAttackF = document.querySelector('#jump-f');
+let buttonJumpAttackA = document.querySelector('#jump-a');
+
+// récupéré les Div principales
 let divStart = document.querySelector('.players-choice');
 let divCombat = document.querySelector('.combat');
 let choice = document.querySelectorAll('div input[type=radio]');
 // console.log(choice);
 
+// evenement Click sur button Start
 buttonStart.addEventListener('click', function (event) {
-
     event.preventDefault();
     let id = 0;
-    let name = document.querySelector('div input[type=text]').value; 
+    let name = document.querySelector('.input-group input[type=text]').value; 
     choice.forEach(item => {
         if (item.checked) {
             id = parseInt(item.getAttribute('id'));
-            console.log(id);
         }
     });
 
-    let fighter = selectFighter(fighters[id][0], id);
+    fighter = selectFighter(fighters[id][0], id);
     id = getRandomInt(3);
-    let adverser = selectFighter(fighters[id][0], id);
-    savePlayers(fighter, adverser);
+    adverser = selectFighter(fighters[id][0], id);
+    // savePlayers(fighter, adverser);
     
     divStart.style.display = 'none';
     divCombat.style.display = 'flex';
-    let img1 =  
     buttonStart.style.display = 'none';
     display();
 })
 
 function display() {
-    let fighter = getPlayer();
-    let adverser = getAdverser();
+    // let fighter = getPlayer();
+    // let adverser = getAdverser();
     let divFight = document.querySelector('.fighter');
+    divFight.innerHTML = '';
     let pictureF = document.createElement('img');
     pictureF.setAttribute("src", `img/${fighter.name}.gif`)
     pictureF.setAttribute("alt", "ryu picture");
@@ -49,15 +71,109 @@ function display() {
 
     pictureF = document.createElement('img');
     pictureF.setAttribute("src", `img/${adverser.name}.gif`)
-    console.log(adverser.name);
     pictureF.setAttribute("alt", "ryu picture");
     pictureF.setAttribute("width", "150");  
     pictureF.setAttribute("height", "300");
     divFight.appendChild(pictureF);
 
-
+    let scoreF = document.querySelector("#score-f");
+    let lifeF = document.querySelector("#life-f");
+    let scoreA = document.querySelector("#score-a");
+    let lifeA = document.querySelector("#life-a");
+    let barreLifeF = document.querySelector("#barre-progress-f");
+    let barreLifeA = document.querySelector("#barre-progress-a");
+    
+    let barrePowerF = document.querySelector("#barre-progress-power-f");
+    let barrePowerA = document.querySelector("#barre-progress-power-a");
+    
+    scoreF.innerHTML = ''; 
+    lifeF.innerHTML = ''; 
+    scoreA.innerHTML = '';
+    lifeA.innerHTML = '';
+    
+    scoreF.textContent = fighter.score; 
+    lifeF.textContent = fighter.life; 
+    scoreA.textContent = adverser.score; 
+    lifeA.textContent = adverser.life; 
+    barreLifeF.style.width = `${fighter.life}%`;
+    barreLifeA.style.width = `${adverser.life}%`;   
+    barrePowerF.style.width = `${fighter.power/4}%`;
+    barrePowerA.style.width = `${adverser.power/4}%`;   
 }
 
+buttonAttackF.addEventListener('click', function (event) {
+    event.preventDefault();
+    // let fighter = getPlayer();
+    // let adverser = getAdverser();
+    fighter.simpleAttack(adverser);
+    // savePlayers(fighter, adverser);
+    if (adverser.life <= 0) {
+        alert("you win");
+        console.log("you win");
+        gameOver();
+    }
+    display();
+});
+
+buttonSuperAttackF.addEventListener('click', function (event) {
+    event.preventDefault();
+    // let fighter = getPlayer();
+    // let adverser = getAdverser();
+    fighter.superAttack(adverser);
+    // savePlayers(fighter, adverser);
+    if (adverser.life <= 0) {
+        alert("you win");
+        console.log("you win");
+        gameOver();
+    }
+    display();
+});
+
+buttonGetPowerF.addEventListener('click', function (event) {
+    event.preventDefault();
+    // let fighter = getPlayer();
+    // let adverser = getAdverser();
+    if (fighter.power < 15) {
+        fighter.getPower(); 
+        console.log('+energy');  
+    } else {
+        alert('you have enough energy');
+    }
+    // savePlayers(fighter, adverser);
+    display();
+});
+
+buttonAttackA.addEventListener('click', function (event) {
+    event.preventDefault();
+    // let fighter = getPlayer();
+    // let adverser = getAdverser();
+    adverser.simpleAttack(fighter);
+    // savePlayers(fighter, adverser);
+    if (fighter.life <= 0) {
+        alert("you lost");
+        console.log("you win");
+        gameOver();
+    }
+    display();
+});
+
+buttonSuperAttackA.addEventListener('click', function (event) {
+    event.preventDefault();
+    // let fighter = getPlayer();
+    // let adverser = getAdverser();
+    adverser.superAttack(fighter);
+    // savePlayers(fighter, adverser);
+    if (fighter.life <= 0) {
+        alert("you lost");
+        console.log("you win");
+        gameOver();
+    }
+    display();
+});
+
+
+
+// les functions
 function selectFighter(name, id) {
     return new Fighter(id, name, fighters[id][1]);
         
@@ -80,6 +196,12 @@ function getPlayer() {
 
 function getAdverser() {
     return JSON.parse(localStorage.getItem("adverser"));
+}
+
+function gameOver() {
+    divStart.style.display = 'block';
+    divCombat.style.display = 'none';
+    buttonStart.style.display = 'block';
 }
 
 
